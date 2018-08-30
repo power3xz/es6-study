82ec4120ed29001f16df7c380c7a7ce74507901d
cheolhoon <power3xz@gmail.com>
2018-08-30 16:46:01 +0900

    change

--- exercise.js
+++ exercise.js
@@ -6,4 +6,9 @@ function* generate(data) {
 
 for (let singleLoop of generate(data)) {
   console.log(singleLoop);
+  // 첫번째 변호사 이름
+  // 첫번째 변호사 사무실 번호
+  // 첫번째 변호사 사무실 이름
+  // 두번째 변호사 이름
+  // ...
 }
